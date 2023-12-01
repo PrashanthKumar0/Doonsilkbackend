608e788207a1ba22cc25e26690c94413f2109f4a
Prakharchaudhary <99970010+Prakharchaudhary@users.noreply.github.com>
2023-12-01 18:12:17 +0530

    Update index.js
    update addProduct route

--- index.js
+++ index.js
@@ -82,20 +82,31 @@ app.post('/Admin_signup', adminController.Admin_signup)
 app.get('/Admin_login', adminController.Admin_login)
 
 
-app.post('/addProductWithVariants', upload.fields([
-    { name: 'image1', maxCount: 1 },
-    { name: 'image2', maxCount: 1 },
-    { name: 'image3', maxCount: 1 },
-]), adminController.addProductWithVariants);
+app.post(
+    '/addProductWithVariants',
+    (req, res, next) => {
+    //   console.log('Before upload.fields middleware:', req.files);
+      next();
+    },
+    upload.fields([
+      { name: 'image1', maxCount: 1 },
+      { name: 'image2', maxCount: 1 },
+      { name: 'image3', maxCount: 1 },
+    ]),
+    (req, res) => {
+    //   console.log('After upload.fields middleware:', req.files);
+      adminController.addProductWithVariants(req, res);
+    }
+  );
 
 app.post('/addVariant', upload.fields([{ name: 'image1', maxCount: 1 }, { name: 'image2', maxCount: 1 }, { name: 'image3', maxCount: 1 }]),
     adminController.addVariant);
 
-app.post('/updateMainProduct', upload.fields([
+   app.post('/updateMainProduct', upload.fields([
     { name: 'image1', maxCount: 1 },
     { name: 'image2', maxCount: 1 },
     { name: 'image3', maxCount: 1 },
-]), adminController.updateMainProduct);
+  ]), adminController.updateMainProduct);
 
 
 app.get('/getMainProductById', adminController.getMainProductById)
